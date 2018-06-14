0d53089908f2985245ebb23f1426e1e62d1b1703
namshot <namshot@gmail.com>
2018-06-14 17:50:30 +0900

    커밋

--- src/main/java/com/threejo/cota/dao/MypageDaoImpl.java
+++ src/main/java/com/threejo/cota/dao/MypageDaoImpl.java
@@ -1,10 +1,13 @@
 package com.threejo.cota.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.threejo.cota.model.Mypage;
+import com.threejo.cota.model.Statistics_TODAY;
 
 @Repository
 public class MypageDaoImpl implements MypageDao {
@@ -20,6 +23,11 @@ public class MypageDaoImpl implements MypageDao {
 	@Override
 	public int updateMyinfo(Mypage member) {
 		return session.update("updateMyinfo", member);
+	}
+
+	@Override
+	public List<Statistics_TODAY> selectListStatToday(String email) {
+		return session.selectList("selectListStatToday", email);
 	}
 	
 }
--- src/main/java/com/threejo/cota/service/MypageServiceImpl.java
+++ src/main/java/com/threejo/cota/service/MypageServiceImpl.java
@@ -1,10 +1,13 @@
 package com.threejo.cota.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.threejo.cota.dao.MypageDao;
 import com.threejo.cota.model.Mypage;
+import com.threejo.cota.model.Statistics_TODAY;
 
 @Service
 public class MypageServiceImpl implements MypageService {
@@ -21,6 +24,11 @@ public class MypageServiceImpl implements MypageService {
 	@Override
 	public int updateMyinfo(Mypage member) {
 		return md.updateMyinfo(member);
+	}
+
+	@Override
+	public List<Statistics_TODAY> selectListStatToday(String email) {
+		return md.selectListStatToday(email);
 	}
 
 }
